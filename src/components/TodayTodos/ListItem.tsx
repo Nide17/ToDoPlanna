7286aa157cc93acc771e7c60cd6635af4d88fcbd
QuizBlogRW <quizblog.rw@gmail.com>
2023-10-17 02:10:16 +0200

    url updated

--- src/components/TodayTodos/ListItem.tsx
+++ src/components/TodayTodos/ListItem.tsx
@@ -7,6 +7,7 @@ const ListItem = ({ list }: any) => {
     return (
         <div className='w-1/2 p-6 flex text-xl text-slate-100 font-bold h-auto bg-slate-100'>
             <div className='p-4 sm:p-8 bg-blue-500 w-full h-fit border border-slate-100 rounded-lg'>
+                
                 {list.title} - {list.status === 'ACTIVE' ? '🕗' : list.status === 'FINISHED' ? '🟢' : '🔴'}
                 <small className='block text-sm text-slate-900 align-bottom'>
                     Due date: {moment(list.dueDate).format('DD/MM/YYYY')} - In {moment(list.dueDate).fromNow()}
@@ -19,9 +20,11 @@ const ListItem = ({ list }: any) => {
                         {
                             list.todos.map((todo: any, index: number) =>
                                 <tr key={index} className="text-lg 2xl:text-2xl font-bold 2xl:mt-5 align-baseline">
+
                                     <td className='text-slate-100 hover:text-slate-900 py-2'>
                                         {index + 1}. {todo.title}&nbsp;&nbsp;
                                     </td>
+
                                     {/* EDIT, DELETE, CHANGE COMPLETE STATUS */}
                                     <td className='flex text-slate-100 hover:text-slate-900 py-2'>
                                         <EditIcon />
